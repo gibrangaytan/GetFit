b509e9e809bfb60d220996af1d522fa196c7f575
Chris <cganda00@gmail.com>
2014-04-18 20:55:50 -0600

    exercise images added in res/drawable-xxhdpi/ exerciseMedia.sql added in GetFit/

---
+++ GetFit/exerciseMedia.sql
@@ -0,0 +1,122 @@
+
+INSERT INTO "exercises_has_media" VALUES (51,1);
+INSERT INTO "exercises_has_media" VALUES (51,2);
+INSERT INTO "exercises_has_media" VALUES (2,3);
+INSERT INTO "exercises_has_media" VALUES (2,4);
+INSERT INTO "exercises_has_media" VALUES (3,5);
+INSERT INTO "exercises_has_media" VALUES (3,6);
+INSERT INTO "exercises_has_media" VALUES (1,7);
+INSERT INTO "exercises_has_media" VALUES (1,8);
+
+INSERT INTO "exercises_has_media" VALUES (7,9);
+INSERT INTO "exercises_has_media" VALUES (7,10);
+INSERT INTO "exercises_has_media" VALUES (6,11);
+INSERT INTO "exercises_has_media" VALUES (6,12);
+INSERT INTO "exercises_has_media" VALUES (9,13);
+INSERT INTO "exercises_has_media" VALUES (9,14);
+INSERT INTO "exercises_has_media" VALUES (10,15);
+INSERT INTO "exercises_has_media" VALUES (10,16);
+
+INSERT INTO "exercises_has_media" VALUES (11,17);
+INSERT INTO "exercises_has_media" VALUES (11,18);
+INSERT INTO "exercises_has_media" VALUES (178,19);
+INSERT INTO "exercises_has_media" VALUES (178,20);
+INSERT INTO "exercises_has_media" VALUES (191,21);
+INSERT INTO "exercises_has_media" VALUES (191,22);
+INSERT INTO "exercises_has_media" VALUES (16,23);
+INSERT INTO "exercises_has_media" VALUES (16,24);
+
+INSERT INTO "exercises_has_media" VALUES (182,25);
+INSERT INTO "exercises_has_media" VALUES (182,26);
+INSERT INTO "exercises_has_media" VALUES (82,27);
+INSERT INTO "exercises_has_media" VALUES (82,28);
+INSERT INTO "exercises_has_media" VALUES (39,29);
+INSERT INTO "exercises_has_media" VALUES (39,30);
+INSERT INTO "exercises_has_media" VALUES (135,31);
+INSERT INTO "exercises_has_media" VALUES (135,32);
+
+INSERT INTO "exercises_has_media" VALUES (41,33);
+INSERT INTO "exercises_has_media" VALUES (41,34);
+INSERT INTO "exercises_has_media" VALUES (50,35);
+INSERT INTO "exercises_has_media" VALUES (50,36);
+INSERT INTO "exercises_has_media" VALUES (94,37);
+INSERT INTO "exercises_has_media" VALUES (94,38);
+
+
+INSERT INTO "media" VALUES (1,bandbackfly1.png);
+INSERT INTO "media" VALUES (2,bandbackfly2.png);
+INSERT INTO "media" VALUES (3,bandcrossover1.png);
+INSERT INTO "media" VALUES (4,bandcrossover2.png);
+INSERT INTO "media" VALUES (5,barbellbenchpress1.png);
+INSERT INTO "media" VALUES (6,barbellbenchpress2.png);
+INSERT INTO "media" VALUES (7,barbellbentoverrow1.png);
+INSERT INTO "media" VALUES (8,barbellbentoverrow2.png);
+
+INSERT INTO "media" VALUES (9,barbellfrontraise1.png);
+INSERT INTO "media" VALUES (10,barbellfrontraise2.png);
+INSERT INTO "media" VALUES (11,barbellgoodmorning1.png);
+INSERT INTO "media" VALUES (12,barbellgoodmorning2.png);
+INSERT INTO "media" VALUES (13,barbellshoulderpress1.png);
+INSERT INTO "media" VALUES (14,barbellshoulderpress2.png);
+INSERT INTO "media" VALUES (15,barbellshrug1.png);
+INSERT INTO "media" VALUES (16,barbellshrug2.png);
+
+INSERT INTO "media" VALUES (17,barbellsquat1.png);
+INSERT INTO "media" VALUES (18,barbellsquat2.png);
+INSERT INTO "media" VALUES (19,barbellwidegripdeclinebenchpress1.png);
+INSERT INTO "media" VALUES (20,barbellwidegripdeclinebenchpress2.png);
+INSERT INTO "media" VALUES (21,bridge1.png);
+INSERT INTO "media" VALUES (22,bridge2.png);
+INSERT INTO "media" VALUES (23,cableinternalrotation1.png);
+INSERT INTO "media" VALUES (24,cableinternalrotation2.png);
+
+INSERT INTO "media" VALUES (25,cablestraightarmpushdown1.png);
+INSERT INTO "media" VALUES (26,cablestraightarmpushdown2.png);
+INSERT INTO "media" VALUES (27,chinup1.png);
+INSERT INTO "media" VALUES (28,chinup2.png);
+INSERT INTO "media" VALUES (29,dumbbellfly1.png);
+INSERT INTO "media" VALUES (30,dumbbellfly2.png);
+INSERT INTO "media" VALUES (31,dumbelllunges1.png);
+INSERT INTO "media" VALUES (32,dumbelllunges2.png);
+
+INSERT INTO "media" VALUES (33,legraise1.png);
+INSERT INTO "media" VALUES (34,legraise2.png);
+INSERT INTO "media" VALUES (35,pushup1.png);
+INSERT INTO "media" VALUES (36,pushup2.png);
+INSERT INTO "media" VALUES (37,superman1.png);
+INSERT INTO "media" VALUES (38,superman2.png);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
