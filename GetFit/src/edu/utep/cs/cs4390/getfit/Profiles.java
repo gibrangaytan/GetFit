a19775541d705cb645e61d5d4212aec1b79b7bf7
miguelpalacios <miguelpalacios@miguels-mbp-2>
2014-04-23 15:47:06 -0600

    Prototype just missing the pictures

---
+++ GetFit/src/edu/utep/cs/cs4390/getfit/Profiles.java
@@ -0,0 +1,15 @@
+package edu.utep.cs.cs4390.getfit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profiles extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profiles);
+        
+        
+    	
+    }
+}
