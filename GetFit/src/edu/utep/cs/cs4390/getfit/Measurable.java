6603bce06235fc0704a6b9fa215ab628acefca71
cganda <cganda00@gmail.com>
2014-05-12 22:16:47 -0600

    Update Measurable.java
    comments added

--- GetFit/src/edu/utep/cs/cs4390/getfit/Measurable.java
+++ GetFit/src/edu/utep/cs/cs4390/getfit/Measurable.java
@@ -1,5 +1,8 @@
 package edu.utep.cs.cs4390.getfit;
-
+/**
+ * used to display data in the profileView. This includes 
+ * measurements for weight, height, waist, chest, neck, arms, and others.
+ */ 
 public class Measurable {
 	private int id;
 	private String name;
@@ -12,21 +15,44 @@ public class Measurable {
 		this.name = name;
 		this.measurement = measurement;
 	}
+	/**
+	 * returns the id of the measurable
+	 * @return the id
+	 */ 
 	public int getId() {
 		return id;
 	}
+	/** sets the id of the object
+	 * @param id the id to set
+	 */ 
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * gets the name of the measurable
+	 * @return the name of the object
+	 */ 
 	public String getName() {
 		return name;
 	}
+	/**
+	 * sets the name of the measurable object
+	 * @param the name to set
+	 */ 
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * gets the measurement of the object
+	 * @return the measurement
+	 */ 
 	public int getMeasurement() {
 		return measurement;
 	}
+	/**
+	 * sets the measurement of the measurable object
+	 * @param the measurement to set
+	 */ 
 	public void setMeasurement(int measurement) {
 		this.measurement = measurement;
 	}
